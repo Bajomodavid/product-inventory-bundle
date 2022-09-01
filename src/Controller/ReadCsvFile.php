cd2b0f05a80f3a6da0c865e1cd7b9754543db950
Bajomo David Mayowa <bajomomayowa@gmail.com>
2022-09-01 14:08:44 +0100

    Updated root directory arguement

--- src/Controller/ReadCsvFile.php
+++ src/Controller/ReadCsvFile.php
@@ -9,12 +9,18 @@ class ReadCsvFile
     private $path;
     private $records;
     private $projectDir;
+    private $adminEmail;
+
+    public function __construct()
+    {
+        $this->projectDir = $this->getParameter('kernel.project_dir');
+        $this->adminEmail = $this->getParameter('product_inventory.manager_email');
+        dd($this->projectDir, $this->adminEmail);
+    }
 
     public function readFile()
     {
-        $this->projectDir = $this->getParameter('kernel.project_dir');
-        $adminEmail = $this->getParameter('product_inventory.manager_email');
-        dd($this->projectDir, $adminEmail);
+        dd($this->projectDir);
     }
 
     public function formatRecords()
