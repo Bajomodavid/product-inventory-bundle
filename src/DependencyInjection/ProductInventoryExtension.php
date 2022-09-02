2781705f73b2bbeb9e86d054ff51d10f05873e7d
Bajomo David Mayowa <bajomomayowa@gmail.com>
2022-09-02 03:48:22 +0100

    Updated Stock Import Doctrice Initializer

--- src/DependencyInjection/ProductInventoryExtension.php
+++ src/DependencyInjection/ProductInventoryExtension.php
@@ -19,6 +19,9 @@ class ProductInventoryExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $container
+            ->register('bajomo_david_product_inventory_bundle.store_stock', 'BajomoDavid\ProductInventoryBundle\Controller\ImportData')
+            ->addArgument('@doctrine.orm.entity_manager');
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
     }
