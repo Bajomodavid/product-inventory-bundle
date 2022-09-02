db272c816b0c6c8e7a9e058e53577c4e28a781aa
Bajomo David Mayowa <bajomomayowa@gmail.com>
2022-09-02 05:24:24 +0100

    Updated Stock Import Doctrice Initializer

--- src/Command/ReadStockData.php
+++ src/Command/ReadStockData.php
@@ -32,7 +32,7 @@ class ReadStockData extends Command
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
 
-        $processRecords = $container->get(ReadCsvFile::class);
+        $processRecords = $container->get('BajomoDavid\ProductInventoryBundle\Controller\ReadCsvFile');
         $records = [];
         try {
             $records = $processRecords->readFile($input->getArgument('path'));
