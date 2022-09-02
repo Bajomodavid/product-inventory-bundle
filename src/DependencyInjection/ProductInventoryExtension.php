231c536875ca1f9e2faf811922bbd62af17eaede
Bajomo David Mayowa <bajomomayowa@gmail.com>
2022-09-02 12:58:18 +0100

    Updated Stock Import Doctrice Initializer

--- src/DependencyInjection/ProductInventoryExtension.php
+++ src/DependencyInjection/ProductInventoryExtension.php
@@ -6,10 +6,12 @@
 
 namespace BajomoDavid\ProductInventoryBundle\DependencyInjection;
 
+use BajomoDavid\ProductInventoryBundle\Controller\ReadCsvFile;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Doctrine\ORM\EntityManager;
 
 /**
  * ProductInventoryExtension
@@ -20,8 +22,8 @@ class ProductInventoryExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $container
-            ->register('bajomo_david_product_inventory_bundle.store_stock', 'BajomoDavid\ProductInventoryBundle\Controller\ImportData')
-            ->addArgument('@doctrine.orm.entity_manager');
+            ->register('app.inventory_read', ReadCsvFile::class)
+            ->addArgument('%kernel.project_dir%');
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
     }
